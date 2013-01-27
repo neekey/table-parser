dc5734177895669b0d1080f555e5867a9e259d04
neekey <ni184775761@gmail.com>
2013-01-27 16:24:10 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 var FS = require( 'fs' );
 var Parser = require( '../lib/index' );
 
-//var data = FS.readFileSync( './wmic.log' ).toString();
-var data = FS.readFileSync( './data' ).toString();
+var win_wmic = './wmic.log';
+var linux_ps = './ps.log';
+
+//var data = FS.readFileSync( linux_ps ).toString();
+var data = FS.readFileSync( win_wmic ).toString();
 var parsedData = Parser.parse( data );
 
 console.log( parsedData );
